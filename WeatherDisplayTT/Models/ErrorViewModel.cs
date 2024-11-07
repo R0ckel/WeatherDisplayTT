0a6c6541db214e4b40361457a2ae359d5d151d59
RomanKoshovyi <romanrokel@gmail.com>
2024-11-07 21:47:59 +0200

    Add project files.

---
+++ WeatherDisplayTT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeatherDisplayTT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
